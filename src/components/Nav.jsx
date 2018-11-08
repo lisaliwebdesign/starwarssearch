751ebd19344e2ca9bbf825212805add6aa64dd85
Lisa Li <lisa.li@oracle.com>
2018-11-08 13:39:31 +0000

    Added missing files

---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  NavBar, // The wrapper for the entire nav (nav element)
+  HomeLink, // A link (a tag) which will take the user home
+  HomeImage, // Image of a house
+  CharName // The name of the Character
+} from "../elements/nav";
+import homeImage from "../images/home.png";
+
+/*
+props:
+- city (string)
+*/
+
+export default class Nav extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <NavBar>
+        <HomeLink to="/">
+          <HomeImage src={homeImage} />
+        </HomeLink>
+        <CharName>{this.props.name}</CharName>
+      </NavBar>
+    );
+  }
+}
